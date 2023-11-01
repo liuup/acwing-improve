e5f1288e7fe924535091d455a0f57652fddd2dc8
UP <akarealup@gmail.com>
2023-11-01 19:20:30 +0800

    modify acwing 188

--- chapter2/acwing188.go
+++ chapter2/acwing188.go
@@ -8,46 +8,10 @@ import (
 
 func _debug() {
 	in := bufio.NewReader(os.Stdin)
-	const eof = 0
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 
 	_ = []interface{}{in}
-
-	_i, _n, buf := 0, 0, make([]byte, 1<<12) // 4KB
-	rc := func() byte {                      // 读一个字符
-		if _i == _n {
-			_n, _ = os.Stdin.Read(buf)
-			if _n == 0 { // EOF
-				return eof
-			}
-			_i = 0
-		}
-		b := buf[_i]
-		_i++
-		return b
-	}
-	ri := func() (x int) { // 读一个整数，支持负数
-		neg := false
-		b := rc()
-		for ; '0' > b || b > '9'; b = rc() {
-			// 某些多组数据的题目，不告诉有多少组数据，那么需要额外判断是否读到了 EOF
-			if b == eof {
-				return
-			}
-			if b == '-' {
-				neg = true
-			}
-		}
-		for ; '0' <= b && b <= '9'; b = rc() {
-			x = x*10 + int(b&15)
-		}
-		if neg {
-			return -x
-		}
-		return
-	}
-	_ = []interface{}{rc, ri}
 
 	// acwing 188
 
