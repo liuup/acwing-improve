4bcfa3856ed3683eda4fd565a1d2774d3d6096ed
UP <akarealup@gmail.com>
2023-11-03 15:56:55 +0800

    modify acwing 1016

--- chapter1/acwing1016.go
+++ chapter1/acwing1016.go
@@ -7,59 +7,24 @@ import (
 )
 
 func _debug() {
-	const eof = 0
+	in := bufio.NewReader(os.Stdin) // bufferio 适用于绝大多数题目
+	_ = []interface{}{in}
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
-	_i, _n, buf := 0, 0, make([]byte, 1<<12) // 4KB
-	rc := func() byte {                      // 读一个字符
-		if _i == _n {
-			_n, _ = os.Stdin.Read(buf)
-			if _n == 0 { // EOF
-				return eof
-			}
-			_i = 0
-		}
-		b := buf[_i]
-		_i++
-		return b
-	}
-	ri := func() (x int) { // 读一个整数，支持负数
-		neg := false
-		b := rc()
-		for ; '0' > b || b > '9'; b = rc() {
-			// 某些多组数据的题目，不告诉有多少组数据，那么需要额外判断是否读到了 EOF
-			if b == eof {
-				return
-			}
-			if b == '-' {
-				neg = true
-			}
-		}
-		for ; '0' <= b && b <= '9'; b = rc() {
-			x = x*10 + int(b&15)
-		}
-		if neg {
-			return -x
-		}
-		return
-	}
-	_ = []interface{}{rc, ri}
 
-	// acwing 1016 最大上升子序列和
+	var k int
+	fmt.Fscan(in, &k)
 
-	n := ri()
-	nums := make([]int, n)
+	nums := make([]int, k)
+	dp := make([]int, k)
+
 	for i := range nums {
-		nums[i] = ri()
+		fmt.Fscan(in, &nums[i])
+		dp[i] = nums[i] // 初始化自己，自己就是一个上升序列
 	}
 
-	ans := 0
-	dp := make([]int, n)
-	for i := range dp {
-		dp[i] = nums[i]
-	}
-
-	for i := 0; i < n; i++ {
+	ans := -1
+	for i := 0; i < k; i++ {
 		for j := 0; j < i; j++ {
 			if nums[j] < nums[i] {
 				dp[i] = max_i(dp[i], dp[j]+nums[i])
