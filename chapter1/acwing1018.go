aac991abc63c5a4a9b53e0622858999962108cee
UP <akarealup@gmail.com>
2023-11-03 09:42:42 +0800

    modify acwing 1018

--- chapter1/acwing1018.go
+++ chapter1/acwing1018.go
@@ -7,9 +7,13 @@ import (
 )
 
 func _debug() {
-	const eof = 0
+	in := bufio.NewScanner(os.Stdin)
+	_ = []interface{}{in}
+
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
+
+	const eof = 0
 	_i, _n, buf := 0, 0, make([]byte, 1<<12) // 4KB
 	rc := func() byte {                      // 读一个字符
 		if _i == _n {
@@ -45,10 +49,7 @@ func _debug() {
 	}
 	_ = []interface{}{rc, ri}
 
-	// acwing 1018
-
 	n := ri()
-
 	nums := make([][]int, n)
 	for i := range nums {
 		nums[i] = make([]int, n)
@@ -57,23 +58,31 @@ func _debug() {
 		}
 	}
 
-	dp := make([][]int, n+1)
+	dp := make([][]int, n)
 	for i := range dp {
-		dp[i] = make([]int, n+1)
+		dp[i] = make([]int, n)
 		for j := range dp[i] {
 			dp[i][j] = 1<<31 - 1
 		}
 	}
 
-	dp[1][1] = nums[0][0]
-	for i := 1; i <= n; i++ {
-		for j := 1; j <= n; j++ {
-			dp[i][j] = min_i(dp[i][j], dp[i-1][j]+nums[i-1][j-1])
-			dp[i][j] = min_i(dp[i][j], dp[i][j-1]+nums[i-1][j-1])
-		}
+	dp[0][0] = nums[0][0]
+
+	// 处理第一行
+	for j := 1; j < n; j++ {
+		dp[0][j] = min_i(dp[0][j], dp[0][j-1]+nums[0][j])
+	}
+	// 第一列
+	for i := 1; i < n; i++ {
+		dp[i][0] = min_i(dp[i][0], dp[i-1][0]+nums[i][0])
 	}
 
-	fmt.Fprintln(out, dp[n][n])
+	for i := 1; i < n; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = min_i(dp[i-1][j], dp[i][j-1]) + nums[i][j]
+		}
+	}
+	fmt.Fprintln(out, dp[n-1][n-1])
 }
 
 func min_i(a, b int) int {
