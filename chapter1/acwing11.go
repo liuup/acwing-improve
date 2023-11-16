caa0d511022610855ea82fd1f6220e95c689a3fa
UP <akarealup@gmail.com>
2023-11-16 16:12:40 +0800

    modify acwing 11

--- chapter1/acwing11.go
+++ chapter1/acwing11.go
@@ -1,3 +1,4 @@
+// local runtime version go1.20.2
 package main
 
 import (
@@ -7,41 +8,68 @@ import (
 )
 
 func _solve() {
-	in := bufio.NewReader(os.Stdin) // bufferio 适用于绝大多数题目
+	const eof = 0
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
+	_i, _n, buf := 0, 0, make([]byte, 1<<12) // 4KB
 
-	ri := func() (a int) { fmt.Fscan(in, &a); return }
-	rs := func() (s string) { fmt.Fscan(in, &s); return }
-	_ = []interface{}{ri, rs}
+	rc := func() byte { // 读一个字符
+		if _i == _n {
+			_n, _ = os.Stdin.Read(buf)
+			if _n == 0 { // EOF
+				return eof
+			}
+			_i = 0
+		}
+		b := buf[_i]
+		_i++
+		return b
+	}
+
+	ri := func() (x int) { // 读一个整数，支持负数
+		neg := false
+		b := rc()
+		for ; '0' > b || b > '9'; b = rc() {
+			// 某些多组数据的题目，不告诉有多少组数据，那么需要额外判断是否读到了 EOF
+			if b == eof {
+				return
+			}
+			if b == '-' {
+				neg = true
+			}
+		}
+		for ; '0' <= b && b <= '9'; b = rc() {
+			x = x*10 + int(b&15)
+		}
+		if neg {
+			return -x
+		}
+		return
+	}
+	_ = []interface{}{ri}
 
 	n, v := ri(), ri()
-	vs := make([]int, n)
-	ws := make([]int, n)
-
-	for i := range vs {
-		vs[i], ws[i] = ri(), ri()
-	}
 
 	dp := make([]int, v+1)
 	cnt := make([]int, v+1) // 背包容积为i时总价值最佳的方案数
-	for i := range cnt {    // 什么都不装也是一种方案数
+	for i := range cnt {
 		cnt[i] = 1
 	}
 
 	const mod = 1e9 + 7
-	for i := 1; i <= n; i++ { // n个物品
-		for j := v; j >= vs[i-1]; j-- {
-			value := dp[j-vs[i-1]] + ws[i-1]
-			if value > dp[j] {
+	for i := 1; i <= n; i++ {
+		vs, ws := ri(), ri()
+
+		for j := v; j >= vs; j-- {
+			value := dp[j-vs] + ws
+			if value > dp[j] { // 新方案价值更大
 				dp[j] = value
-				cnt[j] = cnt[j-vs[i-1]]
+				cnt[j] = cnt[j-vs]
 			} else if value == dp[j] {
-				cnt[j] = (cnt[j] + cnt[j-vs[i-1]]) % mod
+				cnt[j] = (cnt[j] + cnt[j-vs]) % mod
 			}
 		}
 	}
-
 	fmt.Fprintln(out, cnt[v])
 }
 
