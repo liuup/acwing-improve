fabf86e8f3b8a3ea582677b263a167d4b61e3b35
UP <akarealup@gmail.com>
2023-11-28 15:27:47 +0800

    modify acwing 1076

--- chapter2/acwing1076.go
+++ chapter2/acwing1076.go
@@ -1,19 +1,24 @@
+// local runtime version go1.20.2
 package main
 
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
-func _debug() {
-	const eof = 0
-	out := bufio.NewWriter(os.Stdout)
+func _solve(_r io.Reader, _w io.Writer) {
+	_in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
 	defer out.Flush()
+
+	const eof = 0
 	_i, _n, buf := 0, 0, make([]byte, 1<<12) // 4KB
-	rc := func() byte {                      // 读一个字符
+
+	rc := func() byte { // 读一个字符
 		if _i == _n {
-			_n, _ = os.Stdin.Read(buf)
+			_n, _ = _in.Read(buf)
 			if _n == 0 { // EOF
 				return eof
 			}
@@ -23,6 +28,7 @@ func _debug() {
 		_i++
 		return b
 	}
+
 	ri := func() (x int) { // 读一个整数，支持负数
 		neg := false
 		b := rc()
@@ -43,22 +49,8 @@ func _debug() {
 		}
 		return
 	}
-	rs := func() (s []byte) {
-		b := rc()
-		for ; '0' > b || b > '9'; b = rc() { // 'A' 'Z'
-		}
-		// for ; 'a' <= b && b <= 'z'; b = rc() { // 'A' 'Z'
-		// 	s = append(s, b)
-		// }
-		for ; '0' <= b && b <= '9'; b = rc() { // 'A' 'Z'
-			s = append(s, b)
-		}
-		return
-	}
-	_ = []interface{}{rc, ri, rs}
 
 	n := ri()
-
 	grid := make([][]int, n)
 	for i := range grid {
 		grid[i] = make([]int, n)
@@ -66,19 +58,18 @@ func _debug() {
 			grid[i][j] = ri()
 		}
 	}
-
 	vis := make([][]bool, n)
 	for i := range vis {
 		vis[i] = make([]bool, n)
 	}
 
+	type node struct{ i, j int }
 	pre := make([][]node, n)
 	for i := range pre {
 		pre[i] = make([]node, n)
 	}
 
 	q := []node{}
-
 	q = append(q, node{0, 0})
 	vis[0][0] = true
 
@@ -86,47 +77,45 @@ func _debug() {
 		cur := q[0]
 		q = q[1:]
 
-		if cur.x == n-1 && cur.y == n-1 { // final
+		if cur.i == n-1 && cur.j == n-1 {
 			break
 		}
 
 		for _, d := range directions {
-			dx := cur.x + d[0]
-			dy := cur.y + d[1]
+			di := cur.i + d[0]
+			dj := cur.j + d[1]
 
-			if !isok(grid, dx, dy) || grid[dx][dy] == 1 || vis[dx][dy] {
+			if !isok(grid, di, dj) || vis[di][dj] || grid[di][dj] == 1 {
 				continue
 			}
-
-			vis[dx][dy] = true
-			pre[dx][dy] = node{cur.x, cur.y}
-			q = append(q, node{dx, dy})
+			vis[di][dj] = true
+			pre[di][dj] = node{cur.i, cur.j}
+			q = append(q, node{di, dj})
 		}
 	}
 
-	showpath(pre, 0, 0, n-1, n-1, out)
-
-}
-
-func showpath(pre [][]node, x1, y1, x2, y2 int, out *bufio.Writer) {
-	if x2 == x1 && y2 == y1 {
-		fmt.Fprintln(out, x2, y2)
-		return
+	var showpath func(i, j int)
+	showpath = func(i, j int) {
+		if i == 0 && j == 0 { // 终止条件
+			fmt.Fprintln(out, i, j)
+			return
+		}
+		showpath(pre[i][j].i, pre[i][j].j)
+		fmt.Fprintln(out, i, j)
 	}
 
-	showpath(pre, x1, y1, pre[x2][y2].x, pre[x2][y2].y, out)
-	fmt.Fprintln(out, x2, y2)
+	showpath(n-1, n-1)
 }
 
-var directions = [4][2]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}} // 四个遍历方向
+var directions = [][]int{
+	{1, 0}, {-1, 0}, {0, 1}, {0, -1}, // 上下左右
+	// {-1, -1}, {-1, 1}, {1, -1}, {1, 1}, // 斜四方
+}
+
 func isok(grid [][]int, i, j int) bool { // 判断是否在二维数组越界
 	return i >= 0 && i < len(grid) && j >= 0 && j < len(grid[0])
 }
 
-type node struct {
-	x, y int
-}
-
 func main() {
-	_debug()
+	_solve(os.Stdin, os.Stdout)
 }
